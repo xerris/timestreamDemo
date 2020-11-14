4cf9a70bb15801cf1e9280301b64a85d53da20bd
Jason Wiker <jason.wiker@gmail.com>
2020-11-14 08:21:18 -0500

    Initial commit

---
+++ src/TimestreamDemo/TimestreamDemoStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace TimestreamDemo
+{
+    public class TimestreamDemoStack : Stack
+    {
+        internal TimestreamDemoStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
