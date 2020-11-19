7a4f29326fe96578315e23fc89feee810e892f1c
Jason Wiker <jason.wiker@gmail.com>
2020-11-19 08:57:37 -0500

    Add demo iOS project

---
+++ TimestreamDemo/TimestreamDemo/TimestreamDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimestreamDemoApp.swift
+//  TimestreamDemo
+//
+//  Created by Jason Wiker on 2020-11-19.
+//
+
+import SwiftUI
+
+@main
+struct TimestreamDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
